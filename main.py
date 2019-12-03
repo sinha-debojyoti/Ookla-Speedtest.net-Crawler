b9346bec74ff1f5ba0aafa456a0c47be2e673d62
RC-Rajat <rajat08082001@gmail.com>
2019-12-03 17:46:25 +0530

    Added code to show execution time

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import web_crawler
+import time
 
+start = time.time()
 ID = 1000000000
 steps = 10
 
 web_crawler.crawler(ID, steps)
+print("--- %s seconds taken for %s records ---" % ((time.time() - start),steps))
