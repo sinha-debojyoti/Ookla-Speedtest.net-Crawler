3b248ee9902a4e2557907efcdf12d0970fc0eefd
PiranavS <75108937+PiranavS@users.noreply.github.com>
2020-12-07 11:49:04 +0530

    Ray module has been instead of multiprocessing library for faster crawling.
    Ray-module implmentation

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import web_crawler
 if __name__ == '__main__':
     #Starting URL
     ID = 1000000000
-    
+
     # Number of URLS data to be feched
     steps = 10
-    
+
     start = time.time()
-    web_crawler.crawler(ID,steps)
+    web_crawler.crawler.remote(ID,steps)
     print("--- %s seconds taken for %s records ---" % ((time.time() - start),steps))
